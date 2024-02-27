ec55c81e4f4cb2092d4e70d499b3e1146653ba64
Tejas Agrawal <72118095+TejasAgrawal007@users.noreply.github.com>
2024-02-27 10:52:39 +0530

    Update 15_promises.js

--- 15_promises.js
+++ 15_promises.js
@@ -60,3 +60,56 @@ promiseFour
     console.log(error);
   })
   .finally(() => console.log("Finally is either resolve or rjected!"));
+
+
+const promiseFive = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    let error = false;
+
+    if (!error) {
+      resolve({ username: "Javascript", Password: "1234" });
+    } else {
+      reject("JS went Wrong!");
+    }
+  }, 1000);
+});
+
+async function promiseFiveConsumed() {
+  try {
+    const response = await promiseFive;
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+promiseFiveConsumed();
+
+
+
+async function getAllUser() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("Error :", error);
+  }
+}
+
+
+getAllUser();
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+fetch("https://api.github.com/users/TejasAgrawal007")
+  .then((response) => {
+    return response.json();
+  })
+
+  .then((data) => {
+    console.log(data);
+  })
+
+  .catch((error) => {
+    console.log("Error -", error);
+  });
